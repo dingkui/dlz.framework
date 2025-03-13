84de9fbd08851874dd9c22b92ecb0342d0f4a518
丁奎 <59461202@qq.com>
2025-03-13 18:50:41 +0800

    order优化

--- dlz.framework.db/dlz.framework.db.springjdbc/src/main/java/com/dlz/framework/db/modal/result/Order.java
+++ dlz.framework.db/dlz.framework.db.springjdbc/src/main/java/com/dlz/framework/db/modal/result/Order.java
@@ -8,8 +8,6 @@ import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * 排序元素载体
@@ -31,44 +29,41 @@ public class Order implements Serializable {
      * 是否正序排列，默认 true
      */
     private boolean asc = true;
-
-    public static Order asc(String column) {
-        return build(column, true);
-    }
-
-    public static Order desc(String column) {
-        return build(column, false);
-    }
-
-//    public static List<Order> ascs(String... columns) {
-//        return Arrays.stream(columns).map(Order::asc).collect(Collectors.toList());
-//    }
-
-    public static Order[] ascs(String... columns) {
-        return (Order[]) Arrays.stream(columns).map(Order::asc).toArray();
-    }
-    public static <T> Order[] ascs(DlzFn<T, ?>... columns) {
-        return (Order[])Arrays.stream(columns).map(item->Order.asc(DbInfoCache.fnName(item))).toArray();
-    }
-    public static <T1> Order[] descs(DlzFn<T1, ?>... columns) {
-        return (Order[])Arrays.stream(columns).map(item->Order.desc(DbInfoCache.fnName(item))).toArray();
-    }
-
-    public static Order[] descs(String... columns) {
-        return (Order[])Arrays.stream(columns).map(Order::desc).toArray();
-    }
-
-    public static Order[] buildWithSql(String columns) {
-        return (Order[])Arrays.stream(columns.split(",")).map(item->{
-            String[] split = item.split(" ");
-            return build(split[0], split[1]);
-        }).toArray();
-    }
-
     public static Order build(String column, boolean asc) {
         return new Order(column, asc);
     }
     public static Order build(String column, String sort) {
-        return new Order(column, "desc".equalsIgnoreCase(sort));
+        return new Order(column, "asc".equalsIgnoreCase(sort));
+    }
+    public static Order buildWithSql(String sql) {
+        String[] split = sql.split(" ");
+        return build(split[0], split[1]);
+    }
+    public static Order asc(String column) {
+        return build(column, true);
+    }
+    public static <T> Order asc(DlzFn<T, ?> column) {
+        return build(DbInfoCache.fnName(column), true);
+    }
+    public static Order desc(String column) {
+        return build(column, false);
+    }
+    public static <T> Order desc(DlzFn<T, ?> column) {
+        return build(DbInfoCache.fnName(column), false);
+    }
+    public static Order[] ascs(String... columns) {
+        return Arrays.stream(columns).map(Order::asc).toArray(Order[]::new);
+    }
+    public static <T> Order[] ascs(DlzFn<T, ?>... columns) {
+        return Arrays.stream(columns).map(Order::asc).toArray(Order[]::new);
+    }
+    public static Order[] descs(String... columns) {
+        return Arrays.stream(columns).map(Order::desc).toArray(Order[]::new);
+    }
+    public static <T1> Order[] descs(DlzFn<T1, ?>... columns) {
+        return Arrays.stream(columns).map(Order::desc).toArray(Order[]::new);
+    }
+    public static Order[] buildWithSqls(String columns) {
+        return Arrays.stream(columns.split(",")).map(Order::buildWithSql).toArray(Order[]::new);
     }
 }
