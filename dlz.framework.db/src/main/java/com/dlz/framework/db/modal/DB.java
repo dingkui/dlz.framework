f3527a56a7c705b50f48b09bdf13c95cfdff9bd5
丁奎 <59461202@qq.com>
2026-01-27 09:28:04 +0800

    1.移动springjdbc到一级目录，更名dlz.framework.db 2.分离db.mybatis-plus

---
+++ dlz.framework.db/src/main/java/com/dlz/framework/db/modal/DB.java
@@ -0,0 +1,9 @@
+package com.dlz.framework.db.modal;
+
+public class DB {
+    public final static DbJdbc Jdbc = new DbJdbc();
+    public final static DbTable Table = new DbTable();
+    public final static DbSql Sql = new DbSql();
+    public final static DbWrapper Wrapper = new DbWrapper();
+    public final static DbBatch Batch = new DbBatch();
+}
